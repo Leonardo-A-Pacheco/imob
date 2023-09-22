2208d32715c08afe77447f6d8b3624719e6d5c42
Leonardo-A-Pacheco <l00000111.pacheco@gmail.com>
2023-09-21 21:00:30 -0300

    a5

--- wp-content/themes/Overflow/functions.php
+++ wp-content/themes/Overflow/functions.php
@@ -3,4 +3,13 @@
 #link css
 
 wp_enqueue_style('style', get_stylesheet_uri());
+
+#habilitar menu do template
+
+function createMenu(){
+    register_nav_menu('menuSite',__('menu do Site'));
+}
+
+add_action('init','createMenu');
+
 ?>
